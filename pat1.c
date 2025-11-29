b9bd1e9c4e6f9b4ac33487d16026fa01b961241a
Piyush <kazz06n@gmail.com>
2025-11-29 10:16:37 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   pat1.c

--- pat1.c
+++ pat1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 int main (void){
 
     int ar[5]={10,3,6,9,7},par[5]={3,24,4,3,18},far[5];
@@ -10,6 +11,7 @@ int main (void){
             if (ar[i]%par[i]==0){
 
                 far[i]=ar[i];
+                continue;
 
             }
             else{
@@ -17,15 +19,16 @@ int main (void){
                 int s=0;
                 s=par[i]-(ar[i]%par[i]);
                 far[i]=ar[i]+s;
+                continue;
             
             }
-
         }
         else{
 
             if (par[i]%ar[i]==0){
 
                 far[i]=par[i];
+                continue;
 
             }
             else{
@@ -33,15 +36,10 @@ int main (void){
                 int s=0;
                 s=(par[i]%ar[i]);
                 far[i]=par[i]-s;
+                continue;
             
             }
-        
-
-        
-
         }
-
-    
     }
     for (int i=0;i<5;i++ ){
 
